f57f705f2f6124f961b750747edbccd917649ced
alex-gusev <alex-gusev@yandex-team.ru>
2021-03-21 19:19:50 +0300

    simplify demo

--- demo/main.js
+++ demo/main.js
@@ -1,18 +1,18 @@
 import { decodeBlurHash } from '../index.js';
 
-const blurhashElement = document.getElementById('blurhash');
+const blurHashInput = document.getElementById('blurhash');
 const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
 function render() {
-    const blurhash = blurhashElement.value;
-    if (blurhash) {
-        const pixels = decodeBlurHash(blurhash, 32, 32);
-        if (pixels) {
-            const ctx = canvas.getContext('2d');
-            const imageData = new ImageData(pixels, 32, 32);
-            ctx.putImageData(imageData, 0, 0);
-        }
+    const blurHash = blurHashInput.value;
+    if (!blurHash) {
+        return;
     }
+    const pixels = decodeBlurHash(blurHash, 32, 32);
+    const imageData = new ImageData(pixels, 32, 32);
+    ctx.putImageData(imageData, 0, 0);
 }
 
-blurhashElement.addEventListener('keyup', render);
+blurHashInput.addEventListener('input', render);
 render();
